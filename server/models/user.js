ba7e5adb7d20b38bcbb89452ad9634d4d698ff85
minuka <minuka.weerathunga@itelasoft.com>
2018-06-20 10:53:32 +0530

    add post/todos routes and refactor mongoose

---
+++ server/models/user.js
@@ -0,0 +1,17 @@
+var mongoose = require('mongoose');
+var User = mongoose.model('Users',{
+  email: {
+    type:String,
+    required:true,
+    minlength:5,
+    trim:true
+  },
+  name:{
+    type:String,
+    required:true,
+    minlength:1,
+    trim:true
+  }
+});
+
+module.exports ={User};
