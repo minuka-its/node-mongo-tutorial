f246a44577a1f21dc38496b9fad309b39b289fd7
minuka <minuka.weerathunga@itelasoft.com>
2018-06-25 13:32:55 +0530

    Add DELETE /users/me/token route

--- server/server.js
+++ server/server.js
@@ -134,6 +134,17 @@ app.get('/todos',(req,res)=>{
     });
   });
 
+  app.delete('/users/me/token',authenticate, (req,res)=>{
+    req.user.removeToken(req.token).then(()=>{
+      res.status(200).send({
+
+        message:'Logged Out'
+      });
+    },()=>{
+      res.status(400).send();
+    })
+  })
+
 
 
 
